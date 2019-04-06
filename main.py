aaf99be8f20959e5a87998fb86bf1db0bf104170
Jae Kim <kjae>
2019-04-06 10:27:27 -0700

    Updated main

--- main.py
+++ main.py
@@ -11,28 +11,10 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
-@app.route('/donations')
+@app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-
-
-# @app.route('/add_donation', methods=['GET', 'POST'])
-# def add_donation():
-#     # donations = Donation.select()
-#         if request.method == 'POST':
-#             return all()
-#         else:
-#             return render_template('add_donation.jinja2')
-
-    # return render_template('add_donation.jinja2')
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     if request.method == 'POST':
-#         return do_the_login()
-#     else:
-#         return show_the_login_form()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
