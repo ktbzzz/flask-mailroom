0428d4965758485d5652f4694418f55af6c16bdb
Jae Kim <kjae>
2019-04-06 10:17:51 -0700

    updated templates

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -11,17 +11,28 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
-@app.route('/donations/')
+@app.route('/donations')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
 
 
-@app.route('/add_donation/')
+@app.route('/add_donation', methods=['GET', 'POST'])
 def add_donation():
     # donations = Donation.select()
-    return render_template('add_donation.jinja2')
+        if request.method == 'POST':
+            return all()
+        else:
+            return render_template('add_donation.jinja2')
 
+    # return render_template('add_donation.jinja2')
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return do_the_login()
+#     else:
+#         return show_the_login_form()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
